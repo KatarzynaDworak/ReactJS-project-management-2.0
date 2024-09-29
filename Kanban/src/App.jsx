f60b2ea4c4a013324b9c2f70df33554f96300547
Katarzyna <k.dw@poczta.onet.pl>
2024-09-29 17:34:04 +0200

    zustand start implementation - Kanban works with useState()

--- Kanban/src/App.jsx
+++ Kanban/src/App.jsx
@@ -1,23 +1,17 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import { useCounterStore } from './store';
-// import Board from './components/Board'
+import Board from './components/Board'
 import './index.css'
 import Login from './components/Login';
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const count = useCounterStore((state) => state.count);
-  const increment = useCounterStore((state) => state.increment);
-  const decrement = useCounterStore((state) => state.decrement);
+  const [count, setCount] = useState(0)
 
 
   return (
     <>
-    <h1>Count: {count}</h1>
-    <button onClick={increment}>Increment</button>
-    <button onClick={decrement}>Decrement</button>
-      {/* <Board /> */}
-    <Login />
+      <Board />
+      <Login />
     </>
   )
 }
--- Kanban/src/store.js
+++ Kanban/src/store.js
@@ -1,10 +1,7 @@
 import { create } from "zustand";
 
-// Nie potrzebujesz typu CounterStore w JavaScript
 export const useCounterStore = create((set) => ({
-    count: 0,
-    increment: () => set((state) => ({ count: state.count + 1 })), // Dodaje 1 do count
-    decrement: () => set((state) => ({ count: state.count - 1 })), // Odejmuje 1 od count
+    
 }));
 
 // Logika zustand:
