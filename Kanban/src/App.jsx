68b7b7ca139610caff02f1dfa30faca9eb9204cc
Katarzyna <k.dw@poczta.onet.pl>
2024-10-13 14:20:04 +0200

    Button Dashboard renamed

--- Kanban/src/App.jsx
+++ Kanban/src/App.jsx
@@ -24,30 +24,30 @@ function App() {
 
   return (
     <Router>
-      <div className="flex justify-between items-center p-4 bg-gray-800 text-white">
+      <div className="flex justify-between items-center p-4 bg-indigo-900 text-white">
         <p>
           <Link className="text-3xl font-bold" to="/">Management Board</Link>
         </p>
         <nav>
           {/* Przycisk "Login" */}
           {!user && (
-            <Link to="/login" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">Login</Link>
+            <Link to="/login" className="bg-white hover:bg-indigo-200 text-indigo-600 font-semibold py-2 px-4 rounded">Login</Link>
           )}
 
           {/* Przycisk "Register" */}
           {!user && (
-            <Link to="/register" className="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-2 px-4 rounded">Register</Link>
+            <Link to="/register" className="bg-white hover:bg-indigo-200 text-indigo-600 font-semibold py-2 px-4 rounded">Register</Link>
           )}
 
           {/* Przycisk "Dashboard" widoczny tylko po zalogowaniu */}
           {user && (
-            <Link to="/dashboard" className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded">Dashboard</Link>
+            <Link to="/dashboard" className="bg-white hover:bg-indigo-200 text-indigo-600 font-semibold py-2 px-4 rounded">Your projects board</Link>
           )}
         </nav>
 
         {/* Przycisk "Logout" widoczny tylko po zalogowaniu */}
         {user && (
-          <button onClick={logout} className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded">Log out</button>
+          <button onClick={logout} className="bg-white hover:bg-indigo-200 text-indigo-600 font-semibold py-2 px-4 rounded">Log out</button>
         )}
       </div>
 
